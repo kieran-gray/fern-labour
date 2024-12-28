8f905371b83bf351a650d9dc1ceb61cbc7ae253e
Kieran Gray <gray.kieran@protonmail.com>
2024-12-28 17:22:47 +0000

    remove unused auth routes

--- app/presentation/api/routes/router_api_v1.py
+++ app/presentation/api/routes/router_api_v1.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 
-from app.presentation.api.routes.auth import auth_router
 from app.presentation.api.routes.birthing_person import birthing_person_router
 from app.presentation.api.routes.healthcheck import healthcheck_router
 from app.presentation.api.routes.labour import labour_router
@@ -11,7 +10,6 @@ api_v1_router = APIRouter(
 )
 
 api_v1_sub_routers = (
-    auth_router,
     birthing_person_router,
     subscriber_router,
     labour_router,
