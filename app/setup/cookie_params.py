5f62175c58a824243f6a0c2a4fcb999eb21c9058
Kieran Gray <gray.kieran@protonmail.com>
2024-12-29 11:12:43 +0000

    chore: remove unused files

--- app/setup/cookie_params.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from typing import Literal
-
-
-@dataclass(eq=False, slots=True, kw_only=True)
-class CookieParams:
-    secure: bool
-    samesite: Literal["strict", "none"] | None = None
