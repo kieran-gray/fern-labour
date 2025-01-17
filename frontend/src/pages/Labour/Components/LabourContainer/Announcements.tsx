d50b65f36d7740fc2807f12218a385a7f0261a9d
Kieran Gray <34507342+kieran-gray@users.noreply.github.com>
2025-01-17 16:05:19 +0000

    Add announcement history to labour page (#15)
    * Add announcement history to labour page
    
    * remove spacer if no contractions

--- frontend/src/pages/Labour/Components/LabourContainer/Announcements.tsx
+++ frontend/src/pages/Labour/Components/LabourContainer/Announcements.tsx
@@ -1,19 +1,41 @@
 import classes from './LabourContainer.module.css'
 import baseClasses from '../../../../shared-components/shared-styles.module.css'
-import { useState } from 'react';
-import { Stack, Textarea, Title } from '@mantine/core';
+import { useEffect, useRef, useState } from 'react';
+import { ScrollArea, Stack, Textarea, Title } from '@mantine/core';
 import MakeAnnouncementButton from '../Buttons/MakeAnnouncement';
+import { AnnouncementDTO } from '../../../../client';
+import messageClasses from './Announcements.module.css'
 
 
-export function Announcements() {
+export function Announcements({announcementHistory}: {announcementHistory: AnnouncementDTO[]}) {
   const [announcement, setAnnouncement] = useState("");
+  const viewport = useRef<HTMLDivElement>(null);
+
+  const messageBubbles = announcementHistory.map((message) => (
+    <div className={messageClasses.message} id={message.id}>
+      <div className={messageClasses.messageLabel}>{new Date(message.sent_time).toLocaleString().slice(0,17).replace(",", " at")}</div>
+      <div className={messageClasses.messageBubble}>{message.message}</div>
+    </div>
+  ));
+
+  useEffect(() => {
+    if (viewport.current) {
+      viewport.current.scrollTo({top: viewport.current.scrollHeight, behavior: 'auto'});
+    }
+  }, [announcementHistory]);
+
   return (
     <div className={baseClasses.root}>
     <div className={baseClasses.header}>
       <Title fz="xl" className={baseClasses.title}>Make an announcement</Title>
     </div>
     <div className={baseClasses.body}>
-      <Stack align='stretch' justify='center' gap='md'>
+      <Stack align='stretch' justify='center'>
+         {announcementHistory.length > 0 && 
+            <ScrollArea.Autosize mah={300} viewportRef={viewport} p={10}>
+              {messageBubbles}
+            </ScrollArea.Autosize>
+         }
         <Textarea
             radius="lg"
             label="Your announcement"
