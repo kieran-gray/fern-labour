3067471e164aba12cfa6d36985080ac87fad6d8e
Kieran Gray <34507342+kieran-gray@users.noreply.github.com>
2025-01-20 09:57:53 +0000

    Move kafka topic setup to a script (#18)

---
+++ kafka/setup.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+echo -e 'Creating kafka topics'
+
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.labour.begun --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.labour.completed --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.contraction.started --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.contraction.ended --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.subscriber.subscribed-to --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.subscriber.unsubscribed-from --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.labour.announcement-made --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.birthing-person.removed-subscriber --replication-factor 1 --partitions 1
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --create --if-not-exists --topic ${KAFKA_TOPIC_PREFIX}.birthing-person.send-invite --replication-factor 1 --partitions 1
+
+echo -e 'Successfully created the following topics:'
+kafka-topics.sh --bootstrap-server ${KAFKA_BOOTSTRAP_SERVERS} --list
