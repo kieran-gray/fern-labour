9b4ca695184106974a9a0f2ee72b8437633500d4
Kieran Gray <gray.kieran@protonmail.com>
2025-02-12 10:21:04 +0000

    Improve invite container styling

--- frontend/src/pages/Share/Components/InviteContainer/InviteContainer.tsx
+++ frontend/src/pages/Share/Components/InviteContainer/InviteContainer.tsx
@@ -1,12 +1,14 @@
 import { IconAt } from '@tabler/icons-react';
 import { useMutation } from '@tanstack/react-query';
 import { useAuth } from 'react-oidc-context';
-import { Text, TextInput, Title } from '@mantine/core';
+import { Text, TextInput, Title, Image, Group, Space } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { notifications } from '@mantine/notifications';
 import { BirthingPersonService, OpenAPI, SendInviteRequest } from '../../../../client';
 import { SendInviteButton } from '../SendInviteButton/SendInviteButton';
 import baseClasses from '../../../../shared-components/shared-styles.module.css';
+import image from './invite.svg';
+import classes from './InviteContainer.module.css'
 
 export function InviteContainer() {
   const auth = useAuth();
@@ -52,34 +54,46 @@ export function InviteContainer() {
   });
 
   return (
-    <div className={baseClasses.root}>
+    <div className={baseClasses.root} style={{maxWidth: '1100px'}}>
       <div className={baseClasses.header}>
         <Title fz="xl" className={baseClasses.title}>
           Invite
         </Title>
       </div>
       <div className={baseClasses.body}>
-        <Text className={baseClasses.text}>
-          Invite friends and family to allow them to track your labour:
-        </Text>
-        <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
-          <TextInput
-            withAsterisk
-            radius="lg"
-            mt="md"
-            rightSectionPointerEvents="none"
-            rightSection={<IconAt size={16} />}
-            label="Email"
-            placeholder="friend@email.com"
-            key={form.key('email')}
-            size="md"
-            {...form.getInputProps('email')}
-          />
-          <div className={baseClasses.flexRowEnd}>
-            <SendInviteButton />
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Invite friends and family by email
+            </Title>
+            <Text c="var(--mantine-color-gray-7)" mt="md">
+              Invite your friends and family to keep them in the loop and let them track your labour in real time.
+              They’ll be able to support you every step of the way!
+            </Text>
+            <Group mt={30}>
+              <form onSubmit={form.onSubmit((values) => mutation.mutate(values))}>
+                <div className={classes.flexRowEnd} style={{ alignItems: 'end' }}>
+                  <TextInput
+                    withAsterisk
+                    radius="lg"
+                    mt="md"
+                    rightSectionPointerEvents="none"
+                    rightSection={<IconAt size={16} />}
+                    label="Email"
+                    placeholder="friend@email.com"
+                    key={form.key('email')}
+                    size="md"
+                    {...form.getInputProps('email')}
+                  />
+                  <Space w="md" />
+                  <SendInviteButton />
+                </div>
+              </form>
+            </Group>
           </div>
-        </form>
+          <Image src={image} className={classes.image} />
+        </div>
       </div>
     </div>
-  );
+  )
 }
