6984d0ac1b58793386eed97d48b6a9141a970f34
Kieran Gray <gray.kieran@protonmail.com>
2025-03-22 11:31:49 +0000

    update marketing not found page

--- marketing/components/NotFound/Components/Image/Image.tsx
+++ marketing/components/NotFound/Components/Image/Image.tsx
@@ -1,26 +1,39 @@
 import Link from 'next/link';
-import { Button, Container, Image, SimpleGrid, Text, Title } from '@mantine/core';
+import { Button, Container, Image, Text, Title } from '@mantine/core';
 import classes from './Image.module.css';
 
 export function NotFoundImage() {
   return (
+    <div style={{padding: '20px'}}>
     <Container className={classes.root} mt={20}>
-      <SimpleGrid spacing={{ base: 40, sm: 80 }} cols={{ base: 1, sm: 2 }}>
-        <Image src="/images/notFoundImage.svg" className={classes.mobileImage} />
-        <div>
-          <Title className={classes.title}>Something is not right...</Title>
-          <Text c="dimmed" size="lg">
-            Page you are trying to open does not exist. You may have mistyped the address, or the
-            page has been moved to another URL. If you think this is an error contact support.
-          </Text>
-          <Link href="/">
-            <Button variant="outline" size="md" mt="xl" className={classes.control}>
-              Get back to home page
-            </Button>
-          </Link>
+      <div style={{ justifyContent: 'space-between' }}>
+        <Title order={2} visibleFrom="sm">
+          We're not sure how you got here...
+        </Title>
+        <Title order={3} hiddenFrom="sm">
+          We're not sure how you got here...
+        </Title>
+        <div className={classes.imageFlexRow}>
+          <Image src='/images/notFound.svg' className={classes.mobileImage} />
         </div>
-        <Image src="/images/notFoundImage.svg" className={classes.desktopImage} />
-      </SimpleGrid>
+        <Text c="var(--mantine-color-gray-8)" size="lg" mt="30">
+          The page you are trying to open does not exist. You may have mistyped the address, or
+          the page has been moved to another URL. If you think this is an error contact support.
+        </Text>
+        <Link href="/">
+        <Button
+          variant="outline"
+          size="md"
+          mt="xl"
+          radius="xl"
+          className={classes.control}
+        >
+          Lets take you home
+        </Button>
+        </Link>
+      </div>
+      <Image src='/images/notFound.svg' className={classes.desktopImage} />
     </Container>
+    </div>
   );
 }
