3931f25c2a889b86ffedd54d8c94e284e2dbc486
Kieran Gray <gray.kieran@protonmail.com>
2025-05-23 16:12:41 +0100

    Add help documentation for track page

--- frontend/src/pages/Labour/Tabs/Track/Contractions.tsx
+++ frontend/src/pages/Labour/Tabs/Track/Contractions.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
-import { Image, Space, Stack } from '@mantine/core';
-import { useScrollIntoView } from '@mantine/hooks';
+import { IconBook } from '@tabler/icons-react';
+import { ActionIcon, Image, Space, Stack } from '@mantine/core';
+import { useDisclosure, useScrollIntoView } from '@mantine/hooks';
 import { ContractionDTO, LabourDTO } from '../../../../clients/labour_service/index.ts';
 import { ImportantText } from '../../../../shared-components/ImportantText/ImportantText.tsx';
 import { ResponsiveDescription } from '../../../../shared-components/ResponsiveDescription/ResponsiveDescription.tsx';
@@ -11,6 +12,7 @@ import { CallMidwifeAlert } from './Alerts/CallMidwifeAlert.tsx';
 import { GoToHospitalAlert } from './Alerts/GoToHospitalAlert.tsx';
 import { PrepareForHospitalAlert } from './Alerts/PrepareForHospital.tsx';
 import ContractionTimeline from './ContractionTimeline.tsx';
+import { ContractionsHelpModal } from './HelpModal.tsx';
 import StartContractionButton from './StartContractionButton.tsx';
 import { StopwatchHandle } from './Stopwatch/Stopwatch.tsx';
 import image from './Track.svg';
@@ -18,6 +20,7 @@ import baseClasses from '../../../../shared-components/shared-styles.module.css'
 import classes from './Contractions.module.css';
 
 export function Contractions({ labour }: { labour: LabourDTO }) {
+  const [opened, { open, close }] = useDisclosure(false);
   const stopwatchRef = useRef<StopwatchHandle>(null);
   const { scrollIntoView, targetRef } = useScrollIntoView<HTMLDivElement>({
     duration: 200,
@@ -45,17 +48,20 @@ export function Contractions({ labour }: { labour: LabourDTO }) {
   return (
     <div className={baseClasses.root}>
       <div className={baseClasses.body}>
-        <div className={baseClasses.inner} style={{ paddingBottom: 0 }}>
+        <div className={classes.titleRow}>
+          <div className={classes.title} style={{ paddingBottom: 0 }}>
+            <ResponsiveTitle title="Track your contractions" />
+          </div>
+          <ActionIcon radius="lg" variant="light" size="xl" onClick={open}>
+            <IconBook />
+          </ActionIcon>
+          <ContractionsHelpModal close={close} opened={opened} />
+        </div>
+        <div className={baseClasses.inner} style={{ paddingBottom: 0, paddingTop: 0 }}>
           <div className={classes.content}>
-            <div className={classes.titleRow}>
-              <ResponsiveTitle title="Track your contractions" />
-              {/* <Button radius="lg" className={classes.docsButton} leftSection={<IconBook />} variant='light'>
-                Help
-              </Button> */}
-            </div>
             <ResponsiveDescription
               description={completed ? completedDescription : activeDescription}
-              marginTop={10}
+              marginTop={0}
             />
             <Stack align="stretch" justify="flex-end" mt="20px" style={{ alignItems: 'center' }}>
               {sortedContractions.length > 0 && (
