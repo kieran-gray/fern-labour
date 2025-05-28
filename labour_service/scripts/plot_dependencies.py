b35c95636f4734f38d58086a889147e49ad7faab
Kieran Gray <gray.kieran@protonmail.com>
2025-05-28 21:25:30 +0100

    Update dependency plot scripts & generate dependency graphs

--- labour_service/scripts/plot_dependencies.py
+++ labour_service/scripts/plot_dependencies.py
@@ -18,11 +18,18 @@ def generate_dependency_graph_d2(container: AsyncContainer) -> str:
     return dishka.plotter.render_d2(container)
 
 
+def generate_dependency_graph_mermaid(container: AsyncContainer) -> str:
+    """
+    Generates a dependency graph for the container in `d2` format.
+    See https://d2lang.com for rendering instructions.
+    """
+    return dishka.plotter.render_mermaid(container)
+
+
 async def main():
     settings: Settings = Settings.from_file()
-    async with make_plot_data_container(settings)() as container:
-        print(generate_dependency_graph_d2(container))
-        await container.close()
+    container = make_plot_data_container(settings)
+    print(generate_dependency_graph_d2(container))
 
 
 if __name__ == "__main__":
