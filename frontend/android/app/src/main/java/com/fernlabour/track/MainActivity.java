96b9ec5680f526b3e08cd060d3dba846247567e6
Kieran Gray <gray.kieran@protonmail.com>
2025-09-09 21:21:42 +0100

    Capacitor app scafolding

---
+++ frontend/android/app/src/main/java/com/fernlabour/track/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fernlabour.track;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fernlabour.track',
+  appName: 'Fern Labour',
+  webDir: 'dist'
+};
+
+export default config;
